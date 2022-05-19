6ad06d484bcc9f21b0c1d6dc9cf6fac572e86548
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-19 00:35:54 -0300

    refatorando o requisito 1

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Album from '../pages/Album';
-import Favorites from '../pages/Favorites';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Search from '../pages/Search';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
 import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
 import NotFound from '../pages/NotFound';
@@ -11,13 +12,15 @@ class Content extends React.Component {
   render() {
     return (
       <div>
-        <Login />
-        <Search />
-        <Album />
-        <Favorites />
-        <Profile />
-        <ProfileEdit />
-        <NotFound />
+        <BrowserRouter>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/Profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </BrowserRouter>
       </div>
     );
   }
