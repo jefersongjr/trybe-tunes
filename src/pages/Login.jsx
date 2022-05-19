5353ed60655c702d8a1f47c937798b420ed8b8ae
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-18 23:58:12 -0300

    Criando a validação do botão

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import { getUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    nameLogin: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+
+    this.setState(() => ({
+      isDisabled: true,
+    }), this.validateButton);
+  }
+
+  validateButton = () => {
+    const { nameLogin } = this.state;
+    const number = 3;
+    if (nameLogin.length >= number) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  }
+
   render() {
+    const { nameLogin, isDisabled } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <Input
+          type="text"
+          name="nameLogin"
+          value={ nameLogin }
+          handleChange={ this.handleChange }
+          data-testid="login-name-input"
+        />
+        <Button
+          getUser={ getUser }
+          isDisabled={ isDisabled }
+          data-testid="login-submit-button"
+        />
+      </div>
     );
   }
 }
