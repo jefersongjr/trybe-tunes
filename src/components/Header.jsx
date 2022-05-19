e0cda3a3720003548c44acc85793b5fb2ad2c5a0
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-19 19:26:00 -0300

    requisito 4, criando a navegação do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -22,6 +23,11 @@ class Header extends React.Component {
         data-testid="header-component"
         onChange={ this.getUser1 }
       >
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Pesquisa</Link>
+          <Link to="/profile" data-testid="link-to-profile">Pesquisa</Link>
+        </nav>
         { isLoading
           ? (
             <p data-testid="header-user-name">
