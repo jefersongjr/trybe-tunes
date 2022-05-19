529cf713fa68bc076491a9659bec4899296b24d0
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-19 19:03:25 -0300

    requisito 3 , criando o header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Content from './components/Content';
-import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
         <Content />
       </div>
     );
